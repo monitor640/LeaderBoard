adb377913f09374558f2aa9dab99b427a399f3df
kulakarla <sderrtsee@gmail.com>
2023-11-29 15:48:53 +0200

    data fetched

--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -1,36 +1,53 @@
 import React, {useEffect, useState} from 'react';
 
 
-function Leaderboard(){
-    const[leaderboard, setLeaderboard] = useState(null);
-    useEffect(() => {
-        // GET request using fetch inside useEffect React hook
-        fetch('https://crystal-run-prod-api-cgmyl.ondigitalocean.app/api/statistics/leaderboard?epochId=1&take=100')
-            .then(response => response.json())
-            .then(data => setLeaderboard(data.total));
+function Leaderboard() {
+    const [leaderboard, setLeaderboard] = useState(null);
 
-// empty dependency array means this effect will only run once (like componentDidMount in classes)
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://crystal-run-prod-api-cgmyl.ondigitalocean.app/api/statistics/leaderboard?epochId=1&take=100');
+                if (!response.ok) {
+                    console.log("No response")
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+                console.log("Leaderboard data successfully fetched");
+                console.log(data.data);
+                setLeaderboard(data.data);
+            } catch (error) {
+                console.error('Fetch error:', error);
+            }
+        };
+    
+        fetchData();
     }, []);
 
-    const DisplayLeaderboard = leaderboard.map(() => {
-        if (leaderboard)
-        return(
+    console.log("leaderboardddddd");
+    console.log(leaderboard);
+
+    if (!leaderboard) {
+        return (
             <div>
-                <h1>{leaderboard.rank}</h1>
+                <h1>Leaderboard</h1>
+                <div>Loading...</div>
             </div>
         );
-        return (<div>Loading...</div>);
-    });
+    }
 
-
-
-    return(
+    return (
         <div>
             <h1>Leaderboard</h1>
-            <h2>{DisplayLeaderboard}</h2>
+            <div>
+                {leaderboard.map((item, index) => (
+                    <div key={index}>
+                        <h1>{item.rank}</h1>
+                    </div>
+                ))}
+            </div>
         </div>
     );
-
 }
 
 export default Leaderboard;
